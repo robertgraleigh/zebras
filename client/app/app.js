894f2b29e222a4678365b076a4b7a89db92bd349
Merritt Lawrenson <contact@merrittlawrenson.com>
2015-11-14 13:27:39 -0800

    linked to backand

--- client/app/app.js
+++ client/app/app.js
@@ -4,11 +4,15 @@ angular.module('zebraTestApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ui.router'
+  'ui.router',
+  'backand'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, BackandProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+    BackandProvider.setAppName('zebratest');
+    BackandProvider.setSignUpToken('074e7696-4df7-4f46-80f1-6ab827d04b11');
+    BackandProvider.setAnonymousToken('2e994303-7159-47c0-b56e-a989f75414eg');
   });
